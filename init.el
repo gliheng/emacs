7fd651a86a7a37d1743d3142469e4b90e07fd69e
python <python@fandans-MacBook-Air.local>
2016-06-16 18:13:20 +0800

    config multiple-cursors

--- init.el
+++ init.el
@@ -141,7 +141,10 @@
 (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
 (global-set-key (kbd "C->") 'mc/mark-next-word-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-word-like-this)
+(global-set-key (kbd "C-M->") 'mc/skip-to-next-like-this)
+(global-set-key (kbd "C-M-<") 'mc/skip-to-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+(global-set-key (kbd "C-S-<mouse-1>") 'mc/add-cursor-on-click)
 
 ;; auto-complete
 (require 'auto-complete-config)
