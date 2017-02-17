bf79d2be02ae7122333f66a5a63658e297011b6f
clojure <clojure@yangqiaonadeMacBook.local>
2017-02-18 06:44:24 +0800

    added js2-mode

--- init.el
+++ init.el
@@ -155,6 +155,7 @@
   'markdown-mode
   'projectile
   'web-mode
+  'js2-mode
   'protobuf-mode
   'yasnippet
   'multiple-cursors
@@ -199,3 +200,17 @@
 ;; projectile
 (projectile-global-mode)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (js2-mode yasnippet web-mode tagedit protobuf-mode projectile paredit multiple-cursors markdown-mode dracula-theme clojure-mode auto-complete))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
