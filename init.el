e75c1ce2b02309b3553cd163e5c6247541674e40
Amadeus <gliheng@foxmail.com>
2018-09-13 12:10:49 +0800

    jsx file use js2-jsx-mode

--- init.el
+++ init.el
@@ -177,7 +177,7 @@
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.scss\\'" . web-mode))
 
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-jsx-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx?\\'" . js2-jsx-mode))
 
 (editorconfig-mode t)
 
