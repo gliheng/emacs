c6697c1af31a5fc59aee1a9f790490a28c06511f
juju <gliheng@gmail.com>
2019-05-22 09:24:58 +0800

    update projectile config

--- init.el
+++ init.el
@@ -202,6 +202,8 @@
 
 ;; projectile
 (projectile-global-mode)
+(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 
 ;; imenu-anywhere
 (global-set-key (kbd "C-.") 'imenu-anywhere)
